88d9437221c7c038d0abfffc2b417191ecece829
Suporte <lucasmvenero@gmail.com>
2024-10-15 23:18:17 -0300

    first_commit

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '../../assets/vite.svg'
+import './style.css'
+
+function Login() {
+  return (
+    <>
+      <div>
+        <h1>ola mundo</h1>
+      </div>
+    </>
+  )
+}
+
+export default Login
