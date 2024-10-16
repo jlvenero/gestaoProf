c144f1741900e114ccd89f226da3fd6a5d55c630
Suporte <lucasmvenero@gmail.com>
2024-10-15 23:45:59 -0300

    add_folder_class

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/login'
+import Class from './pages/class'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <Class />
   </StrictMode>,
 )
